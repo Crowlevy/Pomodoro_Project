ef57f5d4c2aa44367dd8b92608d18a1249ff25f1
Crowlevy <neckgamer81@gmail.com>
2025-02-04 18:47:52 -0300

    databaseFidx

--- api/config/database.php
+++ api/config/database.php
@@ -3,6 +3,7 @@
 class Database {
     private $host = "tangibly-elegant-veery.data-1.use1.tembo.io";
     private $db_name = "pomodoro_db";
+    private $port = '5432';
     private $username = "postgres";
     private $password = "mBOaU56EjpR9aYgp";
     public $conn;
@@ -10,7 +11,7 @@ class Database {
     public function getConnection() {
         $this->conn = null;
         try {
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn = new PDO("pgsql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $exception) {
             echo "Erro de conexão irmão: " . $exception->getMessage();
